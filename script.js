aa0aa8b11162b1ab11a4a0c87d51f3b956550725
imakasx <akashvishwakarma0618@gmail.com>
2026-07-19 19:31:16 +0530

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+        for (let i = 0; i < as.length; i++) {
+            const element = as[i];
+            if (element.href.endsWith(".m4a")) {
+            songs.push(element.href);
+            }
+        }
+        return songs;
+}
+async function main(){
+    let songs = await getSongs();
+    console.log(songs)
+    let audio = new Audio(songs[0])
+    audio.play();
+}
+main()
